0211e8a3ffd10427b1dc61274d4e99be3f630a39
sumerau <jessie@sumerau.com>
2022-09-28 16:21:23 -0500

    Created folder structure; testing header rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import React from 'react';
 import './App.css';
+
+import {AppHeader} from './appHeader';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <AppHeader/>
     </div>
   );
 }
---
+++ src/appHeader/AppHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AppHeader() {
+  return (
+    <header className="App-Header">
+        <h1>The Wine Cooler Store: In Developement</h1>
+    </header>
+  );
+}
+
+export default App;
