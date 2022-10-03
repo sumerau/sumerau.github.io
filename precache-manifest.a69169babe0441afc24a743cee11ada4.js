e8697b64d5a83966b9a262eda859a06b65530dc7
sumerau <jessie@sumerau.com>
2022-10-03 11:03:03 -0500

    Updates

---
+++ precache-manifest.a69169babe0441afc24a743cee11ada4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f45c36aa28f3280a446",
+    "url": "/static/css/main.a0a987c3.chunk.css"
+  },
+  {
+    "revision": "7f45c36aa28f3280a446",
+    "url": "/static/js/main.1cabf357.chunk.js"
+  },
+  {
+    "revision": "5e16f48e3c311f27a098",
+    "url": "/static/js/runtime~main.2fe9e7f1.js"
+  },
+  {
+    "revision": "d9341d9d62a007e46cf9",
+    "url": "/static/js/2.d4b86167.chunk.js"
+  },
+  {
+    "revision": "2ded26446bc94b81347a",
+    "url": "/static/js/3.398cb210.chunk.js"
+  },
+  {
+    "revision": "3dd736c0d50a617c30d18923c85169d6",
+    "url": "/index.html"
+  }
+];
