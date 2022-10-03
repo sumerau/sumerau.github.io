e3479d6bcf2b14af4878f7d27fa97221d3196114
sumerau <jessie@sumerau.com>
2022-10-03 10:38:09 -0500

    Updates

---
+++ precache-manifest.c20ee03dc027306c83a20f265bad7078.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f45c36aa28f3280a446",
+    "url": "/sumerau/sumerau.github.io.git/static/css/main.a0a987c3.chunk.css"
+  },
+  {
+    "revision": "7f45c36aa28f3280a446",
+    "url": "/sumerau/sumerau.github.io.git/static/js/main.1cabf357.chunk.js"
+  },
+  {
+    "revision": "44acf61475d2d8fc50b9",
+    "url": "/sumerau/sumerau.github.io.git/static/js/runtime~main.7bcbd400.js"
+  },
+  {
+    "revision": "d9341d9d62a007e46cf9",
+    "url": "/sumerau/sumerau.github.io.git/static/js/2.d4b86167.chunk.js"
+  },
+  {
+    "revision": "2ded26446bc94b81347a",
+    "url": "/sumerau/sumerau.github.io.git/static/js/3.398cb210.chunk.js"
+  },
+  {
+    "revision": "7fa01b5c29094f5e0ff067ac76e00a8a",
+    "url": "/sumerau/sumerau.github.io.git/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9a85462ab2819de87204acb5f079072b.js"
+  "/sumerau/sumerau.github.io.git/precache-manifest.c20ee03dc027306c83a20f265bad7078.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/sumerau/sumerau.github.io.git/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
